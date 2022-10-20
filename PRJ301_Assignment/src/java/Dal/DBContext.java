2a66f9a35bf34362b201dfe5b5009e02f412d75e
KayTcode <thanhchhe161893@fpt.edu.vn>
2022-10-20 16:48:48 +0700

    Edit Assignment

--- PRJ301_Assignment/src/java/Dal/DBContext.java
+++ PRJ301_Assignment/src/java/Dal/DBContext.java
@@ -4,10 +4,36 @@
  */
 package Dal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author win
  */
-public class DBContext {
-    
+public abstract class DBContext<T> {
+
+    protected Connection connection;
+
+    public DBContext() throws ClassNotFoundException {
+        try {
+            String user = "thanhchhe161893";
+            String pass = "thanhedo2002";
+            String url = "jdbc:sqlserver://DESKTOP-G2BD9JV:1433;databaseName=PRJ301_Assignment";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public abstract void insert(T model); //INSERT INTO
+    public abstract void update(T model); //UPDATE SET
+    public abstract void delete(T model); //DELETE FROM
+    public abstract T get(int id); //SELECT * FROM WHERE id =
+    public abstract ArrayList<T> list();//SELECT * FROM
 }
--- PRJ301_Assignment/src/java/View/view.java
+++ PRJ301_Assignment/src/java/View/view.java
@@ -4,11 +4,20 @@
  */
 package View;
 
+import Dal.assignment.LecturerDBContext;
+import Dal.assignment.SessionDBContext;
+import Dal.assignment.TimeSlotDBContext;
+import Model.Lecturer;
+import Model.Session;
+import Model.TimeSlot;
+import Util.DateTimeHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  *
@@ -27,6 +36,42 @@ public class view extends HttpServlet{
         processRequest(req, resp);
     }
     void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int lid = Integer.parseInt(req.getParameter("lid"));
+        String raw_from = req.getParameter("from");
+        String raw_to = req.getParameter("to");
+        java.sql.Date from = null;
+        java.sql.Date to = null;
+        if(raw_from ==null || raw_from.length() ==0)
+        {
+            Date today = new Date();
+            int todayOfWeek = DateTimeHelper.getDayofWeek(today);
+            Date e_from = DateTimeHelper.addDays(today, 2 - todayOfWeek);
+            Date e_to = DateTimeHelper.addDays(today, 8-todayOfWeek);
+            from = DateTimeHelper.toDateSql(e_from);
+            to = DateTimeHelper.toDateSql(e_to);
+        }
+        else
+        {
+            from = java.sql.Date.valueOf(raw_from);
+            to = java.sql.Date.valueOf(raw_to);
+        }
+        
+        req.setAttribute("from", from);
+        req.setAttribute("to", to);
+        req.setAttribute("dates", DateTimeHelper.getDateList(from, to));
+        
+        TimeSlotDBContext slotDB = new TimeSlotDBContext();
+        ArrayList<TimeSlot> slots = slotDB.list();
+        req.setAttribute("slots", slots);
+        
+        SessionDBContext sesDB = new SessionDBContext();
+        ArrayList<Session> sessions = sesDB.filter(lid, from, to);
+        req.setAttribute("sessions", sessions);
+        
+        LecturerDBContext lecDB = new LecturerDBContext();
+        Lecturer lecturer = lecDB.get(lid);
+        req.setAttribute("lecturer", lecturer);
+        
         req.getRequestDispatcher("/View_Background/ViewSchedule.jsp").forward(req, resp);
     }
 }
