c59e7cbaf826f3a5bcb2bc81af64f5f5ca97964b
KayTcode <thanhchhe161893@fpt.edu.vn>
2022-11-02 13:53:06 +0700

    edit

--- PRJ301_Assignment_SE1645/src/java/assignmentcontroller/AttandanceController.java
+++ PRJ301_Assignment_SE1645/src/java/assignmentcontroller/AttandanceController.java
@@ -4,29 +4,47 @@
  */
 package assignmentcontroller;
 
+import dal.assignment.SessionDBContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import model.Attandance;
+import model.Session;
+import model.Student;
 
 /**
  *
  * @author win
  */
 public class AttandanceController extends HttpServlet{
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        super.doGet(request, response); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
+        Session ses = new Session();
+        ses.setId(Integer.parseInt(request.getParameter("sesid")));
+        String[] stdids = request.getParameterValues("stdid");
+        for (String stdid : stdids) {
+            Attandance a =new Attandance();
+            Student s = new Student();
+            a.setStudent(s);
+            a.setDescription(request.getParameter("description"+stdid));
+            a.setPresent(request.getParameter("present"+stdid).equals("present"));
+            s.setId(Integer.parseInt(stdid));
+            ses.getAttandances().add(a);
+        }
+        SessionDBContext db = new SessionDBContext();
+        db.update(ses);
+        response.sendRedirect("takeatt?id="+ses.getId());
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
+        int sesid = Integer.parseInt(request.getParameter("id"));
+        SessionDBContext sesDB = new SessionDBContext();
+        Session ses = sesDB.get(sesid);
+        request.setAttribute("ses", ses);
+        request.getRequestDispatcher("../view/AttandanceReport/Attandance.jsp").forward(request, response);
     }
     
 }
